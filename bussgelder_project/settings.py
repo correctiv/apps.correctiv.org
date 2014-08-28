da3d78f870526f6baa1222d4f9cec6c2981b5835
Stefan Wehrmeyer <mail@stefanwehrmeyer.com>
2014-08-28 02:24:46 +0200

    Fix c&p mistake in wsgi file

--- bussgelder_project/settings.py
+++ bussgelder_project/settings.py
@@ -1,5 +1,5 @@
 """
-Django settings for lpgurteile project.
+Django settings for bussgelder_project project.
 
 For more information on this file, see
 https://docs.djangoproject.com/en/1.6/topics/settings/
