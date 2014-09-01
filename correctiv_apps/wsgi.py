26abf37d2405f91787ce4cb9bec700d3c0e3d1fd
Stefan Wehrmeyer <mail@stefanwehrmeyer.com>
2014-09-01 13:14:05 +0200

    Fix WSGI file for deployment

--- correctiv_apps/wsgi.py
+++ correctiv_apps/wsgi.py
@@ -7,8 +7,14 @@ For more information on this file, see
 https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
 """
 
+import sys
 import os
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "correctiv_apps.settings")
+
+project = os.path.dirname(__file__)
+workspace = os.path.join(project, '..')
+sys.path.append(project)
+sys.path.append(workspace)
 
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
